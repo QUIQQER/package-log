cf78d41522425ff9de949ec6b630f95a97065142
Henning Leutz <leutz@pcsg.de>
2014-02-22 11:43:37 +0100

    logs -> verschiedene log levels eingebaut und grundaufbau getätgt

--- lib/QUI/Log/Logger.php
+++ lib/QUI/Log/Logger.php
@@ -1,14 +1,10 @@
 <?php
 
 /**
- * This file contains \QUI\Log\Logger class
+ * This file contains QUI\Log\Logger class
  */
 
 namespace QUI\Log;
-
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\FirePHPHandler;
 
 /**
  * QUIQQER logging service
@@ -18,18 +14,100 @@ use Monolog\Handler\FirePHPHandler;
 
 class Logger
 {
-    static function write($params)
+    /**
+     * Monolog Logger
+     * @var \Monolog\Logger
+     */
+    static $Logger = null;
+
+    /**
+     * Write a message to the logger
+     * event: onLogWrite
+     *
+     * @param String $message - Log message
+     * @param Integer $loglevel - \QUI\System\Log::LEVEL_*
+     */
+    static function write($message, $loglevel=\QUI\System\Log::LEVEL_INFO)
     {
-        $Package = \QUI::getPackageHandler()-getPackage( 'quiqqer/log' );
+        $Logger = self::getLogger();
+        $User   = \QUI::getUserBySession();
 
-        $message  = $params['message'];
-        $loglevel = $params['loglevel'];
+        $context = array(
+            'username' => $User->getName(),
+            'uid'      => $User->getId()
+        );
 
-        $Logger = new Logger( 'quiqqer_logger' );
+        switch ( $loglevel )
+        {
+            case \QUI\System\Log::LEVEL_DEBUG:
+                $Logger->addDebug( $message, $context );
+            break;
+
+            case \QUI\System\Log::LEVEL_INFO:
+                $Logger->addInfo( $message, $context );
+            break;
+
+            case \QUI\System\Log::LEVEL_NOTICE:
+                $Logger->addNotice( $message, $context );
+            break;
+
+            case \QUI\System\Log::LEVEL_WARNING:
+                $Logger->addWarning( $message, $context );
+            break;
+
+            case \QUI\System\Log::LEVEL_ERROR:
+                $Logger->addError( $message, $context );
+            break;
+
+            case \QUI\System\Log::LEVEL_CRITICAL:
+                $Logger->addCritical( $message, $context );
+            break;
+
+            case \QUI\System\Log::LEVEL_ALERT:
+                $Logger->addAlert( $message, $context );
+            break;
+
+            case \QUI\System\Log::LEVEL_EMERGENCY:
+                $Logger->addEmergency( $message, $context );
+            break;
+        }
+    }
+
+    /**
+     * Return the Logger object
+     *
+     * @return \Monolog\Logger
+     */
+    static function getLogger()
+    {
+        if ( self::$Logger ) {
+            return self::$Logger;
+        }
+
+        $PluginManager = \QUI::getPlugins();
+        $Plugin        = $PluginManager->get( 'quiqqer/log' );
+        $Logger        = new \Monolog\Logger( 'QUI:Log' );
+
+        self::$Logger = $Logger;
+
+        try
+        {
+            if ( $Plugin->getSettings('browser_logs', 'firephp' ) ) {
+                $Logger->pushHandler( new \Monolog\Handler\FirePHPHandler() );
+            }
+
+            if ( $Plugin->getSettings('browser_logs', 'chromephp' ) ) {
+                $Logger->pushHandler( new \Monolog\Handler\ChromePHPHandler() );
+            }
+
+        // @todo more handler
 
 
+        } catch ( \QUI\Exception $Exception )
+        {
 
-        $Logger->pushHandler( new FirePHPHandler() );
+        }
 
+        return $Logger;
     }
 }
