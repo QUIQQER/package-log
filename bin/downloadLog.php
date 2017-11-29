d09f7bdd6d82cc72ad0aaa675d14473520f073f2
Jan Wennrich <jan@pcsg.de>
2017-11-29 13:10:10 +0100

    fix: check log download permission before initializing path variables (#6)

--- bin/downloadLog.php
+++ bin/downloadLog.php
@@ -5,21 +5,25 @@ define('QUIQQER_SYSTEM', true);
 $packagesDir = str_replace('quiqqer/log/bin', '', dirname(__FILE__));
 require_once $packagesDir . '/header.php';
 
-$logDir           = VAR_DIR . 'log/';
+if (!QUI::getUserBySession()->getPermission('quiqqer.packages.quiqqerlog.canUse')) {
+    exit;
+}
+
+$logDir = VAR_DIR . 'log/';
 
 $requestedLogName = urldecode(filter_var($_GET['log'], FILTER_SANITIZE_STRING));
 $requestedLogPath = $logDir . $requestedLogName;
 
 if (!file_exists($requestedLogPath)
     || is_dir($requestedLogPath)
-    || dirname($requestedLogPath) . '/' != $logDir  
-    || !QUI::getUserBySession()->isSU()
+    || dirname($requestedLogPath) . '/' != $logDir
 ) {
     exit;
 }
 
 header("Content-Type: text/calendar; charset=utf-8");
 header("Content-Disposition: attachment; filename=\"$requestedLogName\"");
+
 readfile($requestedLogPath);
 
 exit;
