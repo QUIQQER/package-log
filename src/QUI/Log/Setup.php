2b606cf557405f9506e35d6cda3b98ab35e34e64
Jan Wennrich <jan@pcsg.de>
2017-12-06 17:54:35 +0100

    feat: log cleanup cron is added by default on setup (#7)

--- src/QUI/Log/Setup.php
+++ src/QUI/Log/Setup.php
@@ -16,9 +16,30 @@ class Setup
 {
     public static function run()
     {
+        // Create Log Archive Directory
         $logArchiveDir = Manager::LOG_ARCHIVE_DIR;
         if (!is_dir($logArchiveDir)) {
             mkdir($logArchiveDir);
+        }
+
+        // Setup cron for default log deletion every 180 days
+        $CronManager = new QUI\Cron\Manager();
+        $cronName    = QUI::getLocale()->get('quiqqer/log', 'cron.cleanup.delete.title');
+
+        $Config             = QUI::getPackage('quiqqer/log')->getConfig();
+        $isCronAlreadySetup = $Config->getValue('log_cleanup', 'isCronAlreadySetup');
+
+        if (!$CronManager->isCronSetUp($cronName) && !$isCronAlreadySetup) {
+            try {
+                $CronManager->add($cronName, 0, 0, 180, 0, 0);
+
+                $Config->setValue('log_cleanup', 'isCronAlreadySetup', 1);
+                $Config->save();
+            } catch (QUI\Exception $exception) {
+                $msg = QUI::getLocale()->get('quiqqer/log', 'error.setup.cron');
+                QUI\System\Log::addError($msg);
+                QUI::getMessagesHandler()->addError($msg);
+            }
         }
     }
 }
